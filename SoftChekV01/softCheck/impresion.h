ca9a4aa07a270be8e3350b21c3694f34da9b2bb4
nicoamaciel <77804530+nicoamaciel@users.noreply.github.com>
2022-09-18 16:58:06 -0300

    Delete SoftChekV01 directory

--- SoftChekV01/softCheck/impresion.h
+++
@@ -1,9 +0,0 @@
-#ifndef IMPRESION_H_INCLUDED
-#define IMPRESION_H_INCLUDED
- void verEquipos();
- void verOperadores();
- void verImpactos();
- void verAlertas();
-
-
-#endif // IMPRESION_H_INCLUDED
