f0c880a1018cf027abe352a802e468a86ad0f79d
LucaMMonti <94086289+LucaMMonti@users.noreply.github.com>
2022-10-27 14:44:48 -0300

    Update menu.h

--- SoftCheckV03/softcheck3/menu.h
+++ SoftCheckV03/softcheck3/menu.h
@@ -10,9 +10,15 @@ public:
     int altaBaja();
     int consultas();
     int exportar();
+    int subMenuAB(int x, const char*);
+    int subMenuVolver(int posx, int posy);
+    int subMenuCambiarEstado(int posx, int posy, int ip);
+
+    //EDICION DE MENUS
+    void tituloSoft(const char* text, int posx, int posy);
+    void seleccion(const char* text, int posx, int posy, bool selected);
 
 };
-
 
 
 #endif // MENU_H_INCLUDED
